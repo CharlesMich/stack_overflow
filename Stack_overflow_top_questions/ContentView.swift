4b38e9a0c615087fc0e2431660725e4f7e7fead1
CharlesMich <charleedongo@hotmail.com>
2025-02-06 22:42:12 -0600

    Initial Commit

---
+++ Stack_overflow_top_questions/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stack_overflow_top_questions
+//
+//  Created by Charles Michael on 2/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stack_overflow_top_questions/Stack_overflow_top_questionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stack_overflow_top_questionsApp.swift
+//  Stack_overflow_top_questions
+//
+//  Created by Charles Michael on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Stack_overflow_top_questionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
